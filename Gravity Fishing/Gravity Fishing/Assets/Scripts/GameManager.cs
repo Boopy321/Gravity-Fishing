860de8a15102e35da75e7916dd0e02ebaf79510c
Jason Butterfield <butters_jnr@hotmail.com>
2015-05-12 22:09:24 +1000

    This should allow all of us to have the same project and can work on different things at once.

---
+++ Gravity Fishing/Gravity Fishing/Assets/Scripts/GameManager.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+
+
+
+
+	void Awake () {
+		playerScore = 0;
+		enemyScore = 0;
+		if (instance == null)
+			instance = this;
+		else if (instance != this)
+			Destroy (gameObject);
+		
+		if (Fade == null) {
+			Fade = new Texture2D (1, 1);
+			Fade.SetPixel (0, 0, new Color (1, 1, 1, 1));
+		}
+		Screen.orientation = ScreenOrientation.Landscape;
+		SoundManager.Instance.StartGame ();
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+
+	void CreateBlackHole()
+	{
+		Instance
+	}
+
+}
