9f33027ac2a2f688fff3151dbb2ce200dd77cd97
Jason Butterfield <butters_Jnr@hotmail.com>
2015-05-13 12:24:08 +1000

    Clicking works

---
+++ Gravity Fishing/Gravity Fishing/Assets/Scripts/BlackHole.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+
+
+public class BlackHole : MonoBehaviour {
+
+	public PointEffector2D m_gravity;
+	//public GameObject Hook;
+	public ParticleSystem m_particles;
+
+	Blackhole m_object;
+	bool m_Force;
+	bool m_dead;
+
+	void Awake()
+	{
+		m_object.radius = 20.0f;
+		m_object.timer = 10;
+	}
+
+	// Update is called once per frame
+	void Update () 
+	{		
+
+		transform.Rotate (new Vector3 (0, 5, 0));
+		/* BROKEN CODE
+		 * float distance = Vector3.Distance(Hook.gameObject.transform.position,m_object.blackhole.transform.position); 
+			//Apply Gravity to Objects within the Radius
+			if(distance < m_object.radius)
+			{
+				ApplyForce(Hook);
+			}
+		*/
+			//Delete Blackholes after time
+			//if(m_objects[i].timer <= 0)
+			//{
+			//	Destroy(m_objects[i].blackhole)
+			//}
+	}
+
+	void ApplyForce(GameObject a_object)
+	{
+		//Adjust the object position to give it a pull effect to 
+
+	}
+		
+}
--- Gravity Fishing/Gravity Fishing/Assets/Scripts/GameManager.cs
+++ Gravity Fishing/Gravity Fishing/Assets/Scripts/GameManager.cs
@@ -1,42 +1,74 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
+
+public struct Blackhole
+{
+	public GameObject blackhole;
+	public float radius;
+	public int timer;
+}
+
 
 public class GameManager : MonoBehaviour {
 
+	public PointEffector2D m_gravity; // Useless at the moment
+	public GameObject Hook; //Game Hook
+	public ParticleSystem m_particles; //Later on Particle
+	//public Canvas m_clickCanvas;
+	float zdepth;
+	public GameObject m_blackhole; // Actual BlackHole Object
 
-
+	List<GameObject> m_objects;
 
 
 	void Awake () {
-		playerScore = 0;
-		enemyScore = 0;
-		if (instance == null)
-			instance = this;
-		else if (instance != this)
-			Destroy (gameObject);
-		
-		if (Fade == null) {
-			Fade = new Texture2D (1, 1);
-			Fade.SetPixel (0, 0, new Color (1, 1, 1, 1));
-		}
-		Screen.orientation = ScreenOrientation.Landscape;
-		SoundManager.Instance.StartGame ();
+	
+		//Screen.orientation = ScreenOrientation.Landscape;
+		//SoundManager.Instance.StartGame ();
 	}
 
 	// Use this for initialization
 	void Start () {
-	
+		m_objects = new List<GameObject> ();
+		m_blackhole = GameObject.FindGameObjectWithTag("BlackHole");
+	//	m_clickCanvas = new Canvas ();
+		zdepth = 15.0f;
 	}
 	
-	// Update is called once per frame
-	void Update () {
-	
-	}
-
-
-	void CreateBlackHole()
+	void Update () 
 	{
-		Instance
-	}
 
+		//Sphere collider to apply Gravity or not. 
+		for (int i = 0; i < m_objects.Count; i++) 
+		{
+	
+		}
+
+		if(Input.GetMouseButtonDown(0))
+	    {
+			print ("Click");
+
+			GameObject clone;
+			Debug.Log (Input.mousePosition);
+			//Broken Stuff. 
+			Vector3 pos = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, zdepth));
+			//pos *= (distance);
+			Debug.Log (pos);
+			//pos.z = 1;
+			//Clones a blackhole object to worldspace
+			clone = Instantiate(m_blackhole,pos,new Quaternion()) as GameObject;
+			m_objects.Add(clone);
+
+		}
+			//m_objects[i].blackhole;
+			
+			//Delete Blackholes after time
+			//if(m_objects[i].timer <= 0)
+			//{
+			//	Destroy(m_objects[i].blackhole)
+			//}
+
+	}
+	
 }
