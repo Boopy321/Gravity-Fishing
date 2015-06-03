56a73b087a35de6cd6b208c4df473c4e5b459796
Jason Butterfield <butters_Jnr@hotmail.com>
2015-06-03 12:22:05 +1000

    Merge branch 'master' of https://github.com/Boopy321/Gravity-Fishing
    Conflicts:
    	Gravity Fishing/Gravity Fishing/Assets/Models/Planet__normals.tga.meta
    	Gravity Fishing/Gravity Fishing/Gravity Fishing-csharp.sln
    	Gravity Fishing/Gravity Fishing/Gravity Fishing.sln
    	Gravity Fishing/Gravity Fishing/Gravity Fishing.userprefs

--- Gravity Fishing/Gravity Fishing/Assets/Scripts/GameManager.cs
+++ Gravity Fishing/Gravity Fishing/Assets/Scripts/GameManager.cs
@@ -7,7 +7,8 @@ public class GameManager : MonoBehaviour {
 
 	public PointEffector2D m_gravity; // Useless at the moment
 	public GameObject Hook; //Game Hook
-	public ParticleSystem m_particles; //Later on Particle
+	public GameObject Menu;
+
 	public GameObject BlackholePrefab;
 	public BlackHole blackhole;
 	public Vector3 Velocity;
@@ -18,6 +19,7 @@ public class GameManager : MonoBehaviour {
 	List<Vector3> applyForcelist;
 	List<BlackHole> m_objects;
 
+	private bool m_menu;
 	
 	void Awake () {
 	
@@ -32,37 +34,54 @@ public class GameManager : MonoBehaviour {
 		// Force List from the blackholes
 		applyForcelist = new List<Vector3> ();
 		//Velocity = Vector3 (1, 1, 1);
-	
+
+		m_menu = false;
 		var blackholeobject = new GameObject ("Blackhole");
 		blackhole = blackholeobject.AddComponent<BlackHole> ();;
 		m_objects.Add (blackhole);
-
-		Hook = GameObject.FindGameObjectWithTag("Hook");
+	
 	}
 	
 	void Update () 
 	{
 		//Hook.transform.Translate(Velocity * Time.deltaTime);
 		// Clone Blackholes
+		if (m_menu == true) {
+			RunMenu ();
+		}
+		else {
+			RunGame ();
+		}
+
+	}	
+
+	void RunMenu()
+	{
+		Menu.SetActive (true);
+	
+	}
+
+	void RunGame()
+	{
+		Hook.SetActive (true);
 		if (Input.GetMouseButtonDown (0)) 
 		{
 			print ("Click");
-
+			
 			Vector3 pos = Camera.main.ScreenToWorldPoint (new Vector3 (Input.mousePosition.x, Input.mousePosition.y, zdepth));
 			//var blackholeobject = new GameObject ("Blackhole" + m_objects.Count);
 			var clone = Instantiate (BlackholePrefab, pos, new Quaternion ()) as GameObject;
 			if(clone.GetComponent<BlackHole>() == null)
 			{
 				Debug.Log("Clone is Null");
-
+				
 			}
 			else
 			{
 				m_objects.Add(clone.GetComponent<BlackHole>());
 			}
-
 		};
-	 
-	}	
-	
+
+	}
+
 }
--- Gravity Fishing/Gravity Fishing/Assets/Scripts/MenuScene.cs
+++ Gravity Fishing/Gravity Fishing/Assets/Scripts/MenuScene.cs
@@ -1,25 +1,21 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class MenuScene : MonoBehaviour {
 
-	//CURRENTLY WORKING ON DO NOT USE
-	//GUI INIT
-	//void OnGui()
-	//{
-	//	if(GUI.Button(new Rect (30, 30, 150, 30), "Start Game"))
-	//	{
-	//		startGame();
-	//	}
-	//}
-	//
-	//private void startGame()
-	//{
-	//
-	//	print ("Starting Game");
-	//
-	//	DontDestroyOnLoad(Gamestate.Instance());
-	//	Gamestate.Instance ().startState ();
-	//
-	//}
+	public Button play;
+
+	void start()
+	{
+		play.onClick.AddListener (() => {
+			Switch ();});
+
+	}
+
+
+	void Switch()
+	{
+		Application.LoadLevel ("Jarrods");
+	}
 }
